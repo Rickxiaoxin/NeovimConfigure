e14a766cfdd3bafd0c68e7a30639d20669e97da5
WeiShx <2642138150@qq.com>
2025-12-31 23:28:40 +0800

    lazy.nvim加载nohlsearch插件

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    { dir = vim.env.VIMRUNTIME .. "/pack/dist/opt/nohlsearch", event = "BufReadPre" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
