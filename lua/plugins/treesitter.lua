121601a88e44c1eb9e57eadf01f068cef3c6bc79
WeiShx <2642138150@qq.com>
2026-01-08 22:52:27 +0800

    通过build实现tree-sitter-cli的安装

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,16 +3,16 @@ return {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     version = false, -- last release is way too old and doesn't work on Windows
-    build = ":TSUpdate",
     event = "VeryLazy",
-    opts = {},
-    config = function(_, opts)
+    build = function()
       local mr = require("mason-registry")
       if not mr.is_installed("tree-sitter-cli") then
         local p = mr.get_package("tree-sitter-cli")
         p:install()
       end
-      require("nvim-treesitter").setup(opts)
+      local TS = require("nvim-treesitter")
+      TS.update(nil, { summary = true })
     end,
+    opts = {},
   },
 }
