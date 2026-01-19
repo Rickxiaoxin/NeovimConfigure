43ef96e1d0dd51fc57a421b4c69dfaa8adfa5a03
WeiShx <2642138150@qq.com>
2026-01-19 23:10:02 +0800

    自动安装nvim-treesitter中ensure_installed的parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,25 @@
+---@param callback fun()
+local function build(callback)
+  if vim.fn.executable("tree-sitter") == 1 then
+    return callback()
+  end
+
+  local mr = require("mason-registry")
+  if mr.is_installed("tree-sitter-cli") then
+    return callback()
+  end
+
+  mr.refresh(function()
+    local p = mr.get_package("tree-sitter-cli")
+    p:install(
+      nil,
+      vim.schedule_wrap(function()
+        callback()
+      end)
+    )
+  end)
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -5,14 +27,37 @@ return {
     version = false, -- last release is way too old and doesn't work on Windows
     event = "VeryLazy",
     build = function()
-      local mr = require("mason-registry")
-      if not mr.is_installed("tree-sitter-cli") then
-        local p = mr.get_package("tree-sitter-cli")
-        p:install()
-      end
-      local TS = require("nvim-treesitter")
-      TS.update(nil, { summary = true })
+      build(function()
+        local ts = require("nvim-treesitter")
+        ts.update(nil, { summary = true })
+      end)
     end,
-    opts = {},
+    opts = {
+      ensure_installed = {
+        "c",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "vim",
+        "vimdoc",
+        "python",
+      },
+    },
+    config = function(_, opts)
+      local ts = require("nvim-treesitter")
+      local installed = ts.get_installed("parsers")
+      local need_installed = vim
+        .iter(opts.ensure_installed)
+        :filter(function(parser)
+          return not vim.tbl_contains(installed, parser)
+        end)
+        :totable()
+      if #need_installed > 0 then
+        build(function()
+          ts.install(need_installed, { summary = true })
+        end)
+      end
+    end,
   },
 }
